1832bc5afb8e9745feac4c54a699de1d84aa9083
Firdaus <afolabianjola13@gmail.com>
2023-03-16 15:49:45 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle",
+ * followed by a new line.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a[];
+
+	a[] = "Programming is like building a multilingual puzzle"
+		puts(a);
+	puts("\n");
+
+	return (0);
+}
