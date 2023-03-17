334518036d7e2be7d99c7829dfbfc89c0efd6d20
Firdaus <afolabianjola13@gmail.com>
2023-03-17 13:01:11 -0700

    oops

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int g;
+
+	for (g = 0; g < 24; g++)
+	{
+		putchar(g);
+	}
+	putchar('\n');
+	return (0);
+}
