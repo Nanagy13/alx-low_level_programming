92eaa25473cb51285377307a0628c38ddec81d77
Firdaus <afolabianjola13@gmail.com>
2023-03-17 13:13:40 -0700

    llp

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		printf("%d", k);
+	}
+	putchar('\n');
+	return (0);
+}
